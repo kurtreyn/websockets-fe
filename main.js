3e80a3eb7781ba0023cbaa602cd399d867fe338d
kurtreyn <kurt.reynolds@twinspires.com>
2026-05-27 13:01:35 -0400

    finish game

--- main.js
+++ main.js
@@ -1,23 +1,5 @@
 import { createBoard, playMove } from "./connect4.js";
 
-const websocket = new WebSocket("ws://localhost:8001");
-
-function sendMoves(board, websocket) {
-  // When clicking a column, send a "play" event for a move in that column.
-  board.addEventListener("click", ({ target }) => {
-    const column = target.dataset.column;
-    // Ignore clicks outside a column.
-    if (column === undefined) {
-      return;
-    }
-    const event = {
-      type: "play",
-      column: parseInt(column, 10),
-    };
-    websocket.send(JSON.stringify(event));
-  });
-}
-
 function showMessage(message) {
   window.setTimeout(() => window.alert(message), 50);
 }
@@ -41,6 +23,22 @@ function receiveMoves(board, websocket) {
       default:
         throw new Error(`Unsupported event type: ${event.type}.`);
     }
+  });
+}
+
+function sendMoves(board, websocket) {
+  // When clicking a column, send a "play" event for a move in that column.
+  board.addEventListener("click", ({ target }) => {
+    const column = target.dataset.column;
+    // Ignore clicks outside a column.
+    if (column === undefined) {
+      return;
+    }
+    const event = {
+      type: "play",
+      column: parseInt(column, 10),
+    };
+    websocket.send(JSON.stringify(event));
   });
 }
 
